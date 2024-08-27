95bb7cd9f0dd8f7266a535bd546559af324c6c8c
Kevin <lealmighty50@gmail.com>
2024-08-27 13:00:19 +0800

    trained version1 of our model, added new libraries to environment.yml, included a gpt.py which cannot work due to API-Token billing issues

--- Backend/src/utils/fake_news_detector.py
+++ Backend/src/utils/fake_news_detector.py
@@ -1,7 +1,18 @@
 from transformers import pipeline
+import pickle
+
+#function to run for prediction
+def detecting_fake_news(var):    
+#retrieving the best model for prediction call
+    load_model = pickle.load(open('./final_model.sav', 'rb'))
+    prediction = load_model.predict([var])
+    prob = load_model.predict_proba([var])
+
+    return (print("The given statement is ",prediction[0]),
+        print("The truth probability score is ",prob[0][1]))
 
 
-def detect_fake_news(text):
-    classifier = pipeline("text-classification", model="mrm8488/bert-tiny-finetuned-fake-news-detection")
-    result = classifier(text)
-    return result[0]  # e.g., {'label': 'REAL', 'score': 0.99}
+var = input("Please enter the news text you want to verify: ")
+print("You entered: " + str(var))
+
+detecting_fake_news(var)
