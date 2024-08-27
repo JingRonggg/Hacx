4106a39f03a9f82f8a568e62b91ad1829e3c65df
Kevin <94969451+kevintanjc@users.noreply.github.com>
2024-08-27 13:36:41 +0800

    Update fake_news_detector.py

--- Backend/src/utils/fake_news_detector.py
+++ Backend/src/utils/fake_news_detector.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tensorflow as tf
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 
@@ -26,23 +25,3 @@ def interpret_results(scores):
         return "LIKELY TRUE"
     else:
         return "CHECK AGAIN"
-=======
-from transformers import pipeline
-import pickle
-
-#function to run for prediction
-def detecting_fake_news(var):    
-#retrieving the best model for prediction call
-    load_model = pickle.load(open('./final_model.sav', 'rb'))
-    prediction = load_model.predict([var])
-    prob = load_model.predict_proba([var])
-
-    return (print("The given statement is ",prediction[0]),
-        print("The truth probability score is ",prob[0][1]))
-
-
-var = input("Please enter the news text you want to verify: ")
-print("You entered: " + str(var))
-
-detecting_fake_news(var)
->>>>>>> 95bb7cd (trained version1 of our model, added new libraries to environment.yml, included a gpt.py which cannot work due to API-Token billing issues)
