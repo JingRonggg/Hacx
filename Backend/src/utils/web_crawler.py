4671bd63f73fcb8298476f26b2e2f47c9ff91605
negnij <JNG279@e.ntu.edu.sg>
2024-09-01 15:35:37 +0800

    webcrawler using newsplease

--- Backend/src/utils/web_crawler.py
+++ Backend/src/utils/web_crawler.py
@@ -1,4 +1,19 @@
 from newspaper import Article
+from newsplease import NewsPlease
+from newsplease.crawler.spiders.rss_crawler import RssCrawler
+from newsplease.crawler.spiders.newsplease_spider import NewspleaseSpider
+from newsplease.helper_classes.url_extractor import UrlExtractor
+
+RssCrawler(helper=None, url='https://www.channelnewsasia.com/rss', config=None, ignore_regex=1)
+
+def fetch_article2(url):
+    article = NewsPlease.from_url(url)
+    return{
+        'title': print('title: ' + article.title),
+        'description': print('description: ' + article.description),
+        'maintext': print('maintext: ' + article.maintext),
+        'authors': print('authors: '+ article.authors),
+    }
 
 def fetch_article(url):
     article = Article(url)
