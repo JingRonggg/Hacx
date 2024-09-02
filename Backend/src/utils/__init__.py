27d358a27f5cb3bfab49eb08333ef85a630630da
Kevin <lealmighty50@gmail.com>
2024-09-03 01:27:57 +0800

    renamed gpt.py -> gpt_model.py and fake_news_detector.py -> deBERTa_model.py

--- Backend/src/utils/__init__.py
+++ Backend/src/utils/__init__.py
@@ -1,4 +1,4 @@
-from .deBERTa_model import detect_fake_news
+from .deBERTa_model import  detect_fake_news, interpret_results
 from .web_crawler import fetch_article
-from .gpt import get_gpt_response, parse_gpt_response
+from .gpt_model import get_gpt_response, parse_gpt_response
 __all__ = ["get_gpt_response", "parse_gpt_response", "detect_fake_news", "interpret_results", "fetch_article"]
