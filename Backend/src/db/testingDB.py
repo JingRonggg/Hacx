ba48add9f59b90764f4739497a33cf02cb42291a
Kevin <lealmighty50@gmail.com>
2024-09-03 01:26:36 +0800

    This file shows you how to fetch/send/delete data from Azure Database

--- Backend/src/db/testingDB.py
+++ Backend/src/db/testingDB.py
@@ -1,13 +1,26 @@
-#This file contains how the function call should work :)
+# This file contains how the function call should work :)
 
 from db_access import DatabaseAccessAzure
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Configuration
+API_KEY = os.getenv("OPENAI_API_KEY")
+ENDPOINT = os.getenv("OPENAI_API_ENDPOINT")
+SERVER_NAME = os.getenv("SERVER_NAME")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+SERVER_USERNAME = os.getenv("SERVER_USERNAME")
+SERVER_PASSWORD = os.getenv("SERVER_PASSWORD")
 
 # Initialize the DatabaseAccess class with your Azure SQL Database connection details
 db = DatabaseAccessAzure(
-    server_name='fakenewsserver.database.windows.net',  
-    database_name='FakeNews_DB', 
-    username='fakenewsadmin', 
-    password='fakenews1!'  
+    server_name=SERVER_NAME,  
+    database_name=DATABASE_NAME, 
+    username=SERVER_USERNAME, 
+    password=SERVER_PASSWORD  
 )
 
 # # Example of inserting data into the input_data table
