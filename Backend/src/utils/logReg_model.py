cd6bb5d393495e041ff8e26c543839762833e82d
Kevin <lealmighty50@gmail.com>
2024-09-03 13:11:48 +0800

    Updated logReg_model

--- Backend/src/utils/logReg_model.py
+++ Backend/src/utils/logReg_model.py
@@ -4,18 +4,14 @@ from transformers import pipeline
 import pickle
 
 #function to run for prediction
-def detecting_fake_news(var):    
+def logReg_detect_fake_news(var):    
 #retrieving the best model for prediction call
     load_model = pickle.load(open('./final_model.sav', 'rb'))
     prediction = load_model.predict([var])
     prob = load_model.predict_proba([var])
 
-    return (print("The given statement is ",prediction[0]),
-        print("The truth probability score is ",prob[0][1]))
+    return prediction[0], prob[0][1]
 
+    # return (print("The given statement is ",prediction[0]),
+    #     print("The truth probability score is ",prob[0][1]))
 
-var = input("Please enter the news text you want to verify: ")
-print("You entered: " + str(var))
-
-#if you run into "file not found error" you need to change terminal directory to ../utils first
-detecting_fake_news(var)
