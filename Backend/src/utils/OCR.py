edab11a3b8b17d3da8aa81d0ce6798c5da50f34f
jingrongggg <limj0279@e.ntu.edu.sg>
2024-09-06 13:59:49 +0800

    implemented OCR endpoint

---
+++ Backend/src/utils/OCR.py
@@ -0,0 +1,35 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+subscription_key = os.getenv("SUBSCRIPTION_KEY")
+endpoint = os.getenv("ENDPOINT")
+
+# Authenticate the client
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+def azure_ocr_image_to_text(URL):
+    # Perform OCR on the image
+    ocr_result = computervision_client.read(URL, raw=True)
+    # Get the operation location (URL with an ID at the end) from the response
+    operation_location = ocr_result.headers["Operation-Location"]
+    operation_id = operation_location.split("/")[-1]
+
+    # Wait for the operation to complete
+    while True:
+        result = computervision_client.get_read_result(operation_id)
+        if result.status not in ['notStarted', 'running']:
+            break
+
+    # Extract and print the text
+    if result.status == 'succeeded':
+        extracted_text = ""
+        for page in result.analyze_result.read_results:
+            for line in page.lines:
+                extracted_text += line.text + " "
+        return extracted_text
+    else:
+        return "OCR operation failed."
--- Backend/src/utils/__init__.py
+++ Backend/src/utils/__init__.py
@@ -1,5 +1,11 @@
 from .LLMs.deBERTa_model import detect_fake_news, interpret_results
 from .web_crawler import fetch_article
 from .LLMs.gpt_model import get_gpt_response, parse_gpt_response
+from .OCR import azure_ocr_image_to_text
 
-__all__ = ["get_gpt_response", "parse_gpt_response", "detect_fake_news", "interpret_results", "fetch_article"]
+__all__ = ["get_gpt_response", 
+        "parse_gpt_response", 
+        "detect_fake_news", 
+        "interpret_results", 
+        "fetch_article", 
+        "azure_ocr_image_to_text"]
