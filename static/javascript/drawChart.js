28bf821f2c228a904acd1f581d79abc806c50a43
Malcolm <malcolm.limhw@gmail.com>
2024-09-20 14:10:00 +0800

    updated drawChart.js

--- static/javascript/drawChart.js
+++ static/javascript/drawChart.js
@@ -71,33 +71,36 @@ var chart2 = new Chart(ctx2, {
     }
 });
 
-
 var ctx3 = document.getElementById('chart3').getContext('2d');
-    var chart3 = new Chart(ctx3, {
-        type: 'doughnut',
-        data: {
-            labels: Object.keys(interpretationCounts), 
-            datasets: [{
-                label: 'Interpretation Counts',
-                data: Object.values(interpretationCounts), 
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)', 
-                    'rgba(54, 162, 235, 0.2)', 
-                    'rgba(255, 206, 86, 0.2)' 
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-            responsive: true
-        }
-    });
+var chart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        labels: Object.keys(interpretationCounts), 
+        datasets: [{
+            label: 'Interpretation Counts',
+            data: Object.values(interpretationCounts), 
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',   
+                'rgba(54, 162, 235, 0.2)',   
+                'rgba(255, 206, 86, 0.2)',   
+                'rgba(75, 192, 192, 0.2)',  
+                'rgba(153, 102, 255, 0.2)'   
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',     
+                'rgba(54, 162, 235, 1)',     
+                'rgba(255, 206, 86, 1)',     
+                'rgba(75, 192, 192, 1)',     
+                'rgba(153, 102, 255, 1)'     
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        responsive: true
+    }
+});
 
 var ctx4 = document.getElementById('chart4').getContext('2d');
 var chart4 = new Chart(ctx4, {
