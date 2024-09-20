cfe9b6fcfb1520374f32ab202eb4d67f572e96d1
Malcolm <malcolm.limhw@gmail.com>
2024-09-20 09:58:47 +0800

    changed chart frontend

--- static/javascript/drawChart.js
+++ static/javascript/drawChart.js
@@ -71,32 +71,33 @@ var chart2 = new Chart(ctx2, {
     }
 });
 
+
 var ctx3 = document.getElementById('chart3').getContext('2d');
-var chart3 = new Chart(ctx3, {
-    type: 'doughnut',
-    data: {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,  // Allow custom width/height
-        responsive: true,
-        borderColor: "rgba(20,20,20,0.3)"
-    }
-});
+    var chart3 = new Chart(ctx3, {
+        type: 'doughnut',
+        data: {
+            labels: Object.keys(interpretationCounts), 
+            datasets: [{
+                label: 'Interpretation Counts',
+                data: Object.values(interpretationCounts), 
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)', 
+                    'rgba(54, 162, 235, 0.2)', 
+                    'rgba(255, 206, 86, 0.2)' 
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: true
+        }
+    });
 
 var ctx4 = document.getElementById('chart4').getContext('2d');
 var chart4 = new Chart(ctx4, {
